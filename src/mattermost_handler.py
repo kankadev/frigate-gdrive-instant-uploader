b2df856735f7637547e3dc5ea5b0d8d88f3d7cec
Akay <mail@kanka.dev>
2024-05-10 19:59:58 +0300

    Update mattermost_handler.py

--- src/mattermost_handler.py
+++ src/mattermost_handler.py
@@ -1,21 +1,22 @@
 import logging
+import os
+
 import requests
+
+MATTERMOST_PREFIX = os.getenv('MATTERMOST_PREFIX', '')
 
 
 class MattermostHandler(logging.Handler):
     def __init__(self, webhook_url):
-        super().__init__(level=logging.ERROR)  # Set handler level to ERROR
+        super().__init__(level=logging.ERROR)
         self.webhook_url = webhook_url
 
     def emit(self, record):
         try:
             log_entry = self.format(record)
-            payload = {"text": log_entry}
+            prefixed_log_entry = f"{MATTERMOST_PREFIX} {log_entry}"
+            payload = {"text": prefixed_log_entry}
             response = requests.post(self.webhook_url, json=payload)
             response.raise_for_status()
         except Exception as e:
-            print(f"Failed to send log to Mattermost: {e}")
-
-
-# Mattermost Webhook URL
-mattermost_webhook_url = 'https://mattermost.example.com/hooks/your-webhook-url'
+            logging.error(f"Failed to send log to Mattermost: {e}")
