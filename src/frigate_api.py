93e54c817c2ed25a7091c1457616eb46e66ca828
Akay <mail@kanka.dev>
2024-05-12 18:32:01 +0300

    Better error handling and retry failed  uploads

--- src/frigate_api.py
+++ src/frigate_api.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 
 
@@ -5,12 +7,25 @@ def generate_video_url(frigate_url, event_id):
     return f"{frigate_url}/api/events/{event_id}/clip.mp4"
 
 
-# TODO: not in use yet
-def fetch_events(frigate_url, days=3):
-    params = {}
-    if days is not None:
-        params['days'] = days
-    response = requests.get(f'{frigate_url}/api/events', params=params)
-    if response.status_code == 200:
-        return response.json()
-    return None
+def fetch_all_events(frigate_url, batch_size=100):
+    all_events = []
+    before = None
+
+    while True:
+        params = {'limit': batch_size}
+        if before:
+            params['before'] = before
+
+        response = requests.get(f'{frigate_url}/api/events', params=params)
+        if response.status_code == 200:
+            events = response.json()
+            if not events:
+                break  # No more events to fetch
+            all_events.extend(events)
+            before = events[-1]['start_time']
+            logging.debug(f"Fetched {len(events)} events, next 'before' set to {before}")
+        else:
+            logging.error(f"Failed to fetch events: {response.status_code} {response.text}")
+            break
+
+    return all_events
