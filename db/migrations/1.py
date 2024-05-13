3c1524df663e6ce0a04abe1f1ef1d062e017c9fc
Akay <mail@kanka.dev>
2024-05-13 18:56:33 +0300

    Update 1.py

--- db/migrations/1.py
+++ db/migrations/1.py
@@ -3,7 +3,7 @@ import sqlite3
 import os
 
 DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'events.db')
-
+logging.debug(f"DB_PATH: {DB_PATH}")
 
 def apply_migration():
     conn = sqlite3.connect(DB_PATH)
