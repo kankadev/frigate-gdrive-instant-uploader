312d6c62b5f8f581aee5e790a67fe569df21f42c
Akay Berk | kanka.dev <mail@kanka.dev>
2025-07-20 20:26:22 +0300

    Update 2_add_start_time_to_events.py

--- db/migrations/2_add_start_time_to_events.py
+++ db/migrations/2_add_start_time_to_events.py
@@ -1,12 +1,17 @@
 import logging
 import sqlite3
 
-logging.info('Running migration 2_add_start_time_to_events.py...')
-try:
-    cursor.execute('ALTER TABLE events ADD COLUMN start_time REAL')
-    logging.info('Migration 2_add_start_time_to_events.py finished successfully.')
-except sqlite3.OperationalError as e:
-    if 'duplicate column name' in str(e):
-        logging.warning('Column start_time already exists. Skipping migration.')
-    else:
-        raise e
+def apply(cursor):
+    """
+    Applies the migration to add the start_time column to the events table.
+    """
+    try:
+        cursor.execute('ALTER TABLE events ADD COLUMN start_time REAL')
+        logging.info('Migration 2_add_start_time_to_events.py applied successfully.')
+    except sqlite3.OperationalError as e:
+        # This handles the case where the migration is run more than once
+        if 'duplicate column name' in str(e):
+            logging.warning('Column start_time already exists. Skipping migration.')
+        else:
+            # Re-raise the exception if it's a different error
+            raise e
